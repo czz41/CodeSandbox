34324b5d3b05823b31a3740622f6541caed5dc86
czz <1749127251@qq.com>
2025-12-19 09:10:34 +0800

    初始web框架

---
+++ src/main/java/com/zz/zzojcodesandbox/ZzojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.zz.zzojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zz/zzojcodesandbox/ZzojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zz.zzojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
