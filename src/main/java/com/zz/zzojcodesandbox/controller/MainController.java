1c1ab3c6514f16a8fce613a97a59b85ee6171a07
czz <1749127251@qq.com>
2025-12-23 02:17:59 +0800

    提供api调用-简易鉴权

--- src/main/java/com/zz/zzojcodesandbox/controller/MainController.java
+++ src/main/java/com/zz/zzojcodesandbox/controller/MainController.java
@@ -6,10 +6,14 @@ import com.zz.zzojcodesandbox.model.ExecuteCodeResponse;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController("/")
 public class MainController {
 
+    private static final String AUTH_REQUEST_HEADER="auth";
+    private static final String AUTH_REQUEST_SECRET="secretKey";
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
 
@@ -19,7 +23,15 @@ public class MainController {
     }
 
     @PostMapping("/executeCode")
-    ExecuteCodeResponse executeCode(@RequestBody  ExecuteCodeRequest executeCodeRequest){
+    ExecuteCodeResponse executeCode(@RequestBody  ExecuteCodeRequest executeCodeRequest,
+                                    HttpServletRequest request,
+                                    HttpServletResponse response){
+        //基本的认证
+        String authHeader=request.getHeader(AUTH_REQUEST_HEADER);
+        if(!AUTH_REQUEST_SECRET.equals(authHeader)){
+            response.setStatus(403);
+            return null;
+        }
         if(executeCodeRequest==null){
             throw new RuntimeException("请求参数为空");
         }
